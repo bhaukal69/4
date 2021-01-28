ff8b608edb92b961f0af9e2609e9759c847118f1
bhaukal69 <58442832+bhaukal69@users.noreply.github.com>
2021-01-28 21:57:42 +0530

    Update 4.cc

--- 4.cc
+++ 4.cc
@@ -1,126 +1,41 @@
-#include "ns3/core-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/network-module.h"
-#include "ns3/applications-module.h"
-#include "ns3/wifi-module.h"
-#include "ns3/mobility-module.h"
-#include "ns3/internet-module.h"
+import numpy as np
 
+X=np.array(([2,9],[1,5],[3,6]),dtype=float)
+Y=np.array(([92],[85],[89]),dtype=float)
+X=X/np.amax(X)
+Y=Y/100
 
-using namespace ns3;
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+def der_sigmoid(x):
+    return x*(1-x)
 
+epoch=5000
+lr=0.1
 
-int main(int argc, char *argv[])
-{
-    bool verbose = true;
-    uint32_t nWifi = 3;
-    bool tracing = false;
+wh=np.random.uniform(size=(2,3))
+bh=np.random.uniform(size=(1,3))
+wout=np.random.uniform(size=(3,1))
+bout=np.random.uniform(size=(1,1))
 
-    CommandLine cmd;
-    cmd.AddValue("nWifi", "Number of wifi STA devices", nWifi);
-    cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
-    cmd.AddValue("tracing", "Enable pcap tracing", tracing);
-
-    cmd.Parse(argc, argv);
-
-    if (verbose)
-    {
-        LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-        LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
-    }
-
-    NodeContainer p2pNodes;
-    p2pNodes.Create(2);
-
-    PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
-
-    NetDeviceContainer p2pDevices;
-    p2pDevices = pointToPoint.Install(p2pNodes);
-
-    NodeContainer wifiStaNodes;
-    wifiStaNodes.Create(nWifi);
-    NodeContainer wifiApNode = p2pNodes.Get(0);
-
-    YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
-    YansWifiPhyHelper phy = YansWifiPhyHelper::Default();
-    phy.SetChannel(channel.Create());
-
-    WifiHelper wifi;
-    wifi.SetRemoteStationManager("ns3::AarfWifiManager");
-
-    WifiMacHelper mac;
-    Ssid ssid = Ssid("ns-3-ssid");
-    mac.SetType("ns3::StaWifiMac",
-                "Ssid", SsidValue(ssid),
-                "ActiveProbing", BooleanValue(false));
-
-    NetDeviceContainer staDevices;
-    staDevices = wifi.Install(phy, mac, wifiStaNodes);
-
-    mac.SetType("ns3::ApWifiMac",
-                "Ssid", SsidValue(ssid));
-
-    NetDeviceContainer apDevices;
-    apDevices = wifi.Install(phy, mac, wifiApNode);
-
-    MobilityHelper mobility;
-
-    mobility.SetPositionAllocator("ns3::GridPositionAllocator",
-                                  "MinX", DoubleValue(10.0),
-                                  "MinY", DoubleValue(-10.0),
-                                  "DeltaX", DoubleValue(7.0),
-                                  "DeltaY", DoubleValue(12.0),
-                                  "GridWidth", UintegerValue(2),
-                                  "LayoutType", StringValue("RowFirst"));
-
-    mobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
-                              "Bounds", RectangleValue(Rectangle(-50, 50, -50, 50)));
-    mobility.Install(wifiStaNodes);
-
-    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-    mobility.Install(wifiApNode);
-
-    InternetStackHelper stack;
-    stack.Install(p2pNodes.Get(1));
-    stack.Install(wifiApNode);
-    stack.Install(wifiStaNodes);
-
-    Ipv4AddressHelper address;
-
-    address.SetBase("10.1.1.0", "255.255.255.0");
-    Ipv4InterfaceContainer p2pInterfaces;
-    p2pInterfaces = address.Assign(p2pDevices);
-
-    address.SetBase("10.1.3.0", "255.255.255.0");
-    address.Assign(staDevices);
-    address.Assign(apDevices);
-
-    UdpEchoServerHelper echoServer(9);
-
-    ApplicationContainer serverApps = echoServer.Install(p2pNodes.Get(1));
-    serverApps.Start(Seconds(1.0));
-    serverApps.Stop(Seconds(10.0));
-
-    UdpEchoClientHelper echoClient(p2pInterfaces.GetAddress(1), 9);
-    echoClient.SetAttribute("MaxPackets", UintegerValue(1));
-    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
-
-    ApplicationContainer clientApps =
-        echoClient.Install(wifiStaNodes.Get(nWifi - 1));
-    clientApps.Start(Seconds(2.0));
-    clientApps.Stop(Seconds(10.0));
-
-    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
-
-    Simulator::Stop(Seconds(10.0));
-
-    AsciiTraceHelper ascii;
-    pointToPoint.EnableAsciiAll(ascii.CreateFileStream("Tracefilewif.tr"));
-
-    Simulator::Run();
-    Simulator::Destroy();
-    return 0;
-}
+for i in range(epoch):
+    hinp=np.dot(X,wh)+bh
+    h_layer=sigmoid(hinp)
+    outinp=np.dot(h_layer,wout)+bout
+    output=sigmoid(outinp)
+    
+    h_grad=der_sigmoid(h_layer)
+    out_grad=der_sigmoid(output)
+    
+    EO=Y-output
+    d_output=EO*out_grad
+    
+    EH=d_output.dot(wout.T)
+    d_hid=EH*h_grad
+    
+    wout+=h_layer.T.dot(d_output)*lr
+    wh+=X.T.dot(d_hid)*lr
+    
+print(X)
+print(Y)
+print(output)
